dfa3bb41e3da5ae6d96f6572a08db45990a0b134
Andrew Starr <astarr93@outlook.com>
2020-09-20 22:22:48 -0400

    Update App.js
    Added app start and end message

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const questions = require('./utils/questions');
+const startMsg = "Create a simple HTML webpage featuring your Team Directory. Answer the questions below about your squad to get started: "
+const endMsg = "Check your downloads folder for your file."
 
 figlet('Dev Team Summary', function (err, data) {
     if (err) {
         console.log(err);
     }
     else {
-        console.log(`${data}\n${welcomeMessage}`);
+        console.log(`${data}\n${startMsg}`);
     }
     // node.js inquirer package collects user response
     inquirer.prompt(questions).then(answers => {
         const filePath = path.normalize(`${os.homedir}\\downloads\\Test.txt`);
         fs.writeFileSync(filePath, answers);
+        console.log(endMsg);
     })
 })
