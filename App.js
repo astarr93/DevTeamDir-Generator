2e77d4b39da3f24bfc7c91ee5982dec761366e86
Andrew Starr <astarr93@outlook.com>
2020-09-20 23:27:21 -0400

    Removed node packages and tweaked start and end message
    Stopped using node packages os and path and switched the app output to a repo output folder.
    
    Also tweaked the start and end messages

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 const inquirer = require('inquirer');
 const figlet = require('figlet');
 const fs = require('fs');
-const os = require('os');
-const path = require('path');
 const questions = require('./utils/questions');
-const startMsg = "Create a simple HTML webpage featuring your Team Directory. \nAnswer the questions below about your squad to get started: \n";
-const endMsg = "Check your downloads folder for your file.";
+const startMsg = "Create a simple HTML webpage featuring your Team Directory.\n \nAnswer the questions below about your squad to get started: \n";
+const endMsg = "Check the repository's output folder for your file.";
 
+// ascii art style writing to start the UX
 figlet('Dev Team Summary', function (err, data) {
     if (err) {
         console.log(err);
@@ -16,7 +15,7 @@ figlet('Dev Team Summary', function (err, data) {
     }
     // node.js inquirer package collects user response
     inquirer.prompt(questions).then(answers => {
-        const filePath = path.normalize(`${os.homedir}\\downloads\\Test.txt`);
+        const filePath = './output/file.txt';
         fs.writeFileSync(filePath, answers);
         console.log(endMsg);
     })
