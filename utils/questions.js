f12b2d83570ff5871559c93efbfe3cafeceb8724
Andrew Starr <astarr93@outlook.com>
2020-09-21 00:51:49 -0400

    Added new lib files
    Created new library templates to use the app with. Added Employee constructor and updated questions.js

--- utils/questions.js
+++ utils/questions.js
@@ -2,6 +2,23 @@ const questions = [{
     type: "input",
     name: "name",
     message: "What is your name?: "
-}];
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?: "
+},
+{
+    type: "input",
+    name: "pid",
+    message: "Please enter your personnel identification number",
+},
+{
+    type: "list",
+    name: "role",
+    message: "What would you say you do here?: ",
+    choices: ["Engineer, Intern, Manager"]
+},
+];
 
 module.exports = questions;
