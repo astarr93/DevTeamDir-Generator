30a79082ef646ba9a6dd3c93a1ec50210d4fa8c3
Andrew Starr <astarr93@outlook.com>
2020-09-30 03:31:06 -0400

    Update App.js
    Created more const variables to import more components into the app.

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 const inquirer = require('inquirer');
 const figlet = require('figlet');
 const fs = require('fs');
+const path = require("path");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const questions = require('./utils/questions');
 const startMsg = "Create a simple HTML webpage featuring your Team Directory.\n \nAnswer the questions below about your squad to get started: \n";
 const endMsg = "Check the repository's output folder for your file.";
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
 
 // ascii art style writing to start the UX
 figlet('Dev Team Summary', function (err, data) {
