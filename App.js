4780f32a95efd8b392c56d7cf36b2b618cdf0c54
Andrew Starr <astarr93@outlook.com>
2020-10-06 04:30:13 -0400

    Updated app.js and questions.js to make flow easier to follow
    Cleaned up app.js - Separated the flow into manageable functions for easier unit testing. Updated vars and moved code around to make it easier to read.

--- App.js
+++ App.js
@@ -6,24 +6,39 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const questions = require('./lib/Questions');
-const startingQuestions = questions.inital;
 const startMsg = "Create a simple HTML webpage featuring your Team Directory.\n \nAnswer the questions below about your squad to get started: \n";
 const endMsg = "Check the repository's output folder for your file.";
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
+let peopleBank = [];
 
-// ascii art style writing to start the UX
+// ASCCII Art module to write title and start application
 figlet('Dev Team Summary', function (err, data) {
     if (err) {
-        console.log(err);
+        console.log(err1);
     }
     else {
-        console.log(`${data}\n${startMsg}`);
+        console.log(`\n${data}\n${startMsg}`);
     }
-    // node.js inquirer package collects user response, generates output
-    inquirer.prompt(questions.inital).then(answers => {
-        fs.writeFileSync(outputPath, answers);
-        console.log(endMsg);
-    })
+    startApp();
 });
+
+function startApp() {
+
+    // node inquirer package collects user response, generates output
+    getInfo();
+
+
+    function getInfo() {
+        inquirer.prompt(questions).then(answers => {
+            // console.log(Object.keys(answers)); 
+            if (answers.hasMore !== false) {
+                getInfo();
+            }
+        });
+    }
+
+    fs.writeFileSync(outputPath, answers);
+    console.log(endMsg);
+}
