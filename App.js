3a6005b913ef8c5948ac8d9950ce2807f3ed41d6
Andrew Starr <astarr93@outlook.com>
2020-10-06 08:58:11 -0400

    Update App.js
    Added more logic to app.js

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const endMsg = "Check the repository's output folder for your file.";
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-let peopleBank = [];
+const { createInflate } = require('zlib');
 
 // ASCCII Art module to write title and start application
 figlet('Dev Team Summary', function (err, data) {
@@ -25,20 +25,52 @@ figlet('Dev Team Summary', function (err, data) {
 });
 
 function startApp() {
+    const employees = [];
 
     // node inquirer package collects user response, generates output
     getInfo();
 
-
     function getInfo() {
         inquirer.prompt(questions).then(answers => {
             // console.log(Object.keys(answers)); 
+            addPerson(answers);
             if (answers.hasMore !== false) {
                 getInfo();
             }
+            managerCheck();
         });
-    }
+    };
 
-    fs.writeFileSync(outputPath, answers);
-    console.log(endMsg);
+    function managerCheck(employees) {
+        const hasManager = (Element) => Element.role === "Manager";
+
+        if (employees.some(hasManager) !== true) {
+            console.log("You need at least 1 employee with a Manager role to get your file!");
+            getInfo();
+        }
+
+        else {
+            fs.writeFileSync(outputPath, answers);
+            console.log(endMsg);
+        }
+
+    };
+
+    function addPerson(answers) {
+        switch (answers.role) {
+            case "Engineer":
+                const e = new Engineer([...answers]);
+                employees.push(e);
+                break;
+            case "Intern":
+                const i = new Intern([...answers]);
+                employees.push(i);
+                break;
+            case "Manager":
+                const m = new Manager([...answers]);
+                employees.push(m);
+                break;
+        }
+        console.log(employees);
+    }
 }
